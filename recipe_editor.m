99c0efc119b33eca7ef1700c0dcbd7cf47b7fbd5
Po-Nan Li <lishantreasure@gmail.com>
2014-11-03 20:13:54 +0800

    add Iph into recipe

--- recipe_editor.m
+++ recipe_editor.m
@@ -4,6 +4,9 @@ clear all;
 recipe_name = 'SACLA201406';
 %% Physical constants
 
+Iph = 6.225e36; % [phs/m^2/sec]
+dt = 10e-15;
+
 % detector 1
 N1 = 2399;
 lam = 0.31e-9;
@@ -13,7 +16,7 @@ dx1 = lam * z1 / N1 / du;
 
 % detector 2
 N2 = 1100;
-z1 = 2.7;
+z2 = 2.7;
 dx2 = lam * 2 / N2 / du;
 
 %% Missing pixels
